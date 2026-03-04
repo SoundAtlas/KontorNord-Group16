e2a9979150cad6d4c09db51c0cf8a6a39be80c2b
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-03-04 19:52:43 +0100

    Add BookingService with booking storage and retrieval

--- KontorNord/Services/BookingService.cs
+++ KontorNord/Services/BookingService.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using KontorNord.Models;
+
 namespace KontorNord.Services
 {
-	public class Class1
+	// Class responsible for managing bookings in the system - stores bookings and provides methods to add and retrieve bookings
+	public class BookingService
 	{
-		public Class1()
+		// List that stores bookings
+		private List<Booking> _bookings = new();
+
+		// Adds a new booking 		
+		public Booking AddBooking(Booking booking)
 		{
+			_bookings.Add(booking);
+			return booking;
 		}
+
+		// Returns all bookings currently stored
+		public List<Booking> GetAllBookings()
+		{
+			return _bookings;
+		}
+
 	}
 }
