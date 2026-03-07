2a808cdf1bb3d87ff096964ff609328f6661e65a
VehementWhyrm <emmerencevml@outlook.com>
2026-03-07 22:50:59 +0100

    Add  UI folder with ConsoleHelpers and Menu. Refactor : move console UI logic into UI folder and slim down Program.cs

---
+++ KontorNord/KN/KN/UI/ConsoleHelpers.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KN.UI
+{
+    internal class ConsoleHelpers
+    {
+        public static void FlushKeyBuffer(string[] titleScreen)
+        {
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+        }
+
+        public static void RenderCenteredBlock(string[] lines)
+        {
+            int blockHeight = lines.Length;
+            int startY = (Console.WindowHeight - blockHeight) / 2;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int x = (Console.WindowWidth - lines[i].Length) / 2;
+                int y = startY + i;
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(lines[i]);
+            }
+        }
+        
+        public static int ChooseFromList(string title, string[] options)
+        {
+            int selected = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(title + "\n");
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selected)
+                    {
+                        Console.WriteLine($"> {options[i]} <");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" {options[i]} ");
+
+                    }
+                }
+
+                var key = Console.ReadKey(true).Key;
+                int lastIndex = options.Length - 1;
+
+                if (key == ConsoleKey.UpArrow)
+                {
+                    selected--;
+
+                    if (selected < 0)
+                    {
+                        selected = lastIndex;
+                    }
+
+
+                }
+
+                if (key == ConsoleKey.DownArrow)
+                {
+                    selected++;
+
+                    if (selected > lastIndex)
+                    {
+                        selected = 0;
+                    }
+                }
+
+                if (key == ConsoleKey.Enter)
+                {
+                    return selected;
+                }
+
+
+
+
+
+            }
+        }
+
+        public static void ShowTitleScreen(int milliseconds)
+        {
+            string[] titleScreen =
+            {
+                @$"╔═══════════════════════════════════════════════════════════════════════════════╗",
+                @$"║                                                                               ║",
+                @$"║            ██████╗  ██████╗  ██████╗ ██╗  ██╗██╗███╗   ██╗ ██████╗            ║",
+                @$"║            ██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝██║████╗  ██║██╔════╝            ║",
+                @$"║            ██████╔╝██║   ██║██║   ██║█████╔╝ ██║██╔██╗ ██║██║  ███╗           ║",
+                @$"║            ██╔══██╗██║   ██║██║   ██║██╔═██╗ ██║██║╚██╗██║██║   ██║           ║",
+                @$"║            ██████╔╝╚██████╔╝╚██████╔╝██║  ██╗██║██║ ╚████║╚██████╔╝           ║",
+                @$"║            ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝            ║",
+                @$"║                                                                               ║",
+                @$"║     _  __  ___   _   _  _____   ___   ____     _   _   ___   ____   ____      ║",
+                @$"║    | |/ / / _ \ | \ | ||_   _| / _ \ |  _ \   | \ | | / _ \ |  _ \ |  _ \     ║",
+                @$"║    | ' / | | | ||  \| |  | |  | | | || |_) |  |  \| || | | || |_) || | | |    ║",
+                @$"║    | . \ | |_| || |\  |  | |  | |_| ||  _ <   | |\  || |_| ||  _ < | |_| |    ║",
+                @$"║    |_|\_\ \___/ |_| \_|  |_|   \___/ |_| \_\  |_| \_| \___/ |_| \_\|____/     ║",
+                @$"║                                                                               ║",
+                @$"║                                                                               ║",
+                @$"╚═══════════════════════════════════════════════════════════════════════════════╝",
+
+                    };
+
+            ConsoleHelpers.RenderCenteredBlock(titleScreen);
+            Console.CursorVisible = false;
+            Thread.Sleep(milliseconds);
+            Console.Clear();
+            ConsoleHelpers.FlushKeyBuffer(titleScreen);
+
+        }
+    }
+}
---
+++ KontorNord/KN/KN/UI/Menu.cs
@@ -0,0 +1,132 @@
+﻿using KN.Models;
+using KN.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KN.UI
+{
+    internal class Menu
+    {
+        public static Medarbejder MedarbejderSelection(BookingSystem system)
+        {
+            List<Medarbejder> medarbejdere = system.GetMedarbejdere();
+            string[] options = new string[medarbejdere.Count];
+
+            for (int i = 0; i < medarbejdere.Count; i++)
+            {
+                options[i] = $"{medarbejdere[i].medarbejderId}. {medarbejdere[i].navn}";
+            }
+
+            int selectedIndex = ConsoleHelpers.ChooseFromList("VAELG MEDARBEJDER:", options);
+
+            return medarbejdere[selectedIndex];
+
+        }
+
+        public static Moedelokale MoedelokaleSelection(BookingSystem system)
+        {
+            List<Moedelokale> moedelokaler = system.GetMoedelokaler();
+            string[] options = new string[moedelokaler.Count];
+
+            for (int i = 0; i < moedelokaler.Count; i++)
+            {
+                options[i] = $"{moedelokaler[i].moedelokaleId}. {moedelokaler[i].navn}";
+            }
+
+            int selectedIndex = ConsoleHelpers.ChooseFromList("VAELG MOEDELOKALE:", options);
+            return moedelokaler[selectedIndex];
+        }
+
+        public static void MenuMain(BookingSystem system)
+        {
+
+            string[] menuMain =
+            {
+                    "NY BOOKING",
+                    "SE BOOKINGER",
+                    "AFSLUT",
+                    };
+
+            while (true)
+            {
+                int choice = ConsoleHelpers.ChooseFromList("HOVEDMENU", menuMain);
+
+                if (choice == 0)
+                {
+                    while (true)
+                    {
+
+                        Medarbejder valgtMedarbejder = Menu.MedarbejderSelection(system);
+
+                        Console.Clear();
+                        Console.WriteLine($"{valgtMedarbejder.navn}");
+
+                        Console.Clear();
+
+                        string[] medarbejderConfirmation =
+                        {
+                        "JA",
+                        "NEJ",
+                    };
+
+
+                        int choiceconfirmMedarbejder = ConsoleHelpers.ChooseFromList($"MEDARBEJDER: {valgtMedarbejder.navn}\n\nBEKRAEFT?", medarbejderConfirmation);
+
+                        if (choiceconfirmMedarbejder == 0)
+                        {
+                            while (true)
+                            {
+                                Moedelokale valgtMoedelokale = Menu.MoedelokaleSelection(system);
+
+                                Console.Clear();
+                                Console.WriteLine($"{valgtMoedelokale.navn}");
+
+                                Console.Clear();
+
+                                string[] moedelokaleConfirmation =
+                                {
+                               "JA",
+                               "NEJ",
+                            };
+
+                                int choiceconfirmMoedelokale = ConsoleHelpers.ChooseFromList($"MOEDELOKALE: {valgtMoedelokale.navn}\n\nBEKRAEFT?", moedelokaleConfirmation);
+
+                                if (choiceconfirmMoedelokale == 0)
+                                {
+                                    //To be implemented
+                                }
+                                else if (choiceconfirmMoedelokale == 1)
+                                {
+                                    continue;
+                                }
+
+                                break;
+                            }
+                        }
+                        else if (choiceconfirmMedarbejder == 1)
+                        {
+                            continue;
+                        }
+                    }
+                }
+
+                else if (choice == 1)
+                {
+                    Moedelokale valgtMoedelokale = Menu.MoedelokaleSelection(system);
+
+                    Console.Clear();
+                    Console.WriteLine($"{valgtMoedelokale.navn}");
+                    Console.ReadKey();
+                }
+
+                else if (choice == 2)
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
