01b22823cf98532a4434fdb6ee74ada2e6c724f6
Anika <anika.fuglsang@gmail.com>
2026-03-07 13:35:18 +0100

    Add loop to keep menu running until user exits

--- KontorNord/Program.cs
+++ KontorNord/Program.cs
@@ -4,16 +4,53 @@
     {
         static void Main(string[] args)
         {
-           List<Room> rooms = new List<Room>();
+            List<Room> rooms = new List<Room>();
 
             rooms.Add(new Room("Alpha", 10));
             rooms.Add(new Room("Beta", 8));
             rooms.Add(new Room("Gamma", 20));
 
-            foreach (Room room in rooms)
+            bool running = true;
+
+            while (running)
+
             {
-                Console.WriteLine("Room: "+ room.Name + "Capacity: " + room.Capacity);
+
+                Console.WriteLine("KontorNord System");
+                Console.WriteLine("1 - Show Rooms");
+                Console.WriteLine("2 - Exit");
+
+
+                string choice = Console.ReadLine();
+
+
+
+                if (choice == "1")
+
+                {
+
+                    foreach (Room room in rooms)
+                    {
+                        Console.WriteLine("Room: " + room.Name + " Capacity: " + room.Capacity);
+                    }
+
+                }
+                else if (choice == "2")
+
+                {
+                 running = false;
+                    Console.WriteLine("Program Closed.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Invaild Choice. ");
+                }
+
+                Console.WriteLine();
+            }
 
         }
     }
 }
+
