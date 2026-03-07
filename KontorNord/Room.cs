cd7eae62b10cb45e51fd6feaf729d5c4d6e7a53b
Anika <anika.fuglsang@gmail.com>
2026-03-07 12:49:37 +0100

    Add Room class

---
+++ KontorNord/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KontorNord
+{
+    internal class Room
+    {
+        public string Name { get; set; } = "";
+        public int Capacity { get; set; }
+    }
+}
