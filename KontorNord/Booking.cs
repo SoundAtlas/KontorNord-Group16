28e8da416c96e23c222b96f9beb95d82682d5af5
Anika <anika.fuglsang@gmail.com>
2026-03-08 12:35:15 +0100

    Add Id property to Booking class

--- KontorNord/Booking.cs
+++ KontorNord/Booking.cs
@@ -6,13 +6,15 @@ namespace KontorNord
 {
     internal class Booking
     {
+        public int Id { get; set; }
         public string RoomName { get; set; } = "";
             public string BookedBy { get; set; } = "";
         public string Date { get; set; } = "";
 
-        public Booking(string roomName , string bookedBy, string date)
+        public Booking(int id, string roomName , string bookedBy, string date)
         {
 
+            Id = id;
             RoomName = roomName;
             BookedBy = bookedBy;
             Date = date;
